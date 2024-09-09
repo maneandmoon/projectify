96b0c7b7d3670444d2cf48e0e49698620cc02339
Ryan <coap.ryanmontgomery@gmail.com>
2024-09-09 12:38:31 -0600

    updated routes for error handling

--- server/app.py
+++ server/app.py
@@ -9,13 +9,14 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
 from models import User, Project, Interest, UserInterest, ProjectInterest, Comment
+from sqlalchemy.exc import IntegrityError
 
 # Views go here!
 
@@ -24,6 +25,83 @@ def index():
     return '<h1>Project Server</h1>'
 
 
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    if request.method == 'GET':
+        users = User.query.all()
+        return make_response([user.to_dict() for user in users], 200)
+
+    elif request.method == 'POST':
+        data = request.get_json()
+
+        # Check if username already exists
+        existing_user = User.query.filter_by(username=data.get("username")).first()
+        if existing_user:
+            return make_response(({"error": "Username already exists"}), 400)
+
+        # Check if email already exists
+        existing_email = User.query.filter_by(email=data.get("email")).first()
+        if existing_email:
+            return make_response(({"error": "Email already exists"}), 400)
+
+        try:
+            new_user = User(
+                username=data.get("username"),
+                email=data.get("email"),
+                password=data.get("password"),
+                bio=data.get("bio"),
+                avatar=data.get("avatar")
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            return make_response(new_user.to_dict(), 201)
+        except IntegrityError:
+            db.session.rollback()
+            return make_response(({"error": "An error occurred while creating the user"}), 500)
+
+@app.errorhandler(IntegrityError)
+def handle_integrity_error(e):
+    db.session.rollback()
+    return make_response(({"error": "An integrity error occurred"}), 400)
+
+@app.route('/users/<int:id>', methods=['GET', 'DELETE', 'PATCH'])
+def user_by_id(id):
+    user = User.query.filter(User.id == id).first()
+    #Check if user does not exist
+    if not user:
+        return make_response({"error": "User not found"}, 404)
+
+    if request.method == 'GET':
+        return make_response(user.to_dict(), 200)
+
+    elif request.method == 'DELETE':
+        db.session.delete(user)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "User deleted."
+        }
+
+        return make_response(response_body, 200)
+
+    elif request.method == 'PATCH':
+        data = request.get_json()
+
+        for attr, value in data.items():
+            setattr(user, attr, value)
+
+        try:
+            db.session.commit()
+            return make_response(user.to_dict(), 200)
+        #Error handler for unsuccessful user update
+        except IntegrityError:
+            db.session.rollback()
+            return make_response({"error": "An error occurred while updating the user"}, 400)
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
 
--- server/models.py
+++ server/models.py
@@ -1,6 +1,5 @@
 
 from config import db
-
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -8,8 +7,8 @@ class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
     password = db.Column(db.String(120), nullable=False)
     bio = db.Column(db.String(500))
     avatar = db.Column(db.String(200))
@@ -17,11 +16,10 @@ class User(db.Model, SerializerMixin):
     interests = db.relationship('UserInterest', back_populates='user')
     interest_names = association_proxy('interests', 'interest_name')
 
-    serialize_rules = ('-projects.user', '-interests.user')
+    serialize_rules = ('-projects.user', '-interests.user', '-projects.comments', '-projects.interests')
 
 class Project(db.Model, SerializerMixin):
     __tablename__ = 'projects'
-    serialize_rules = ('-user.projects', '-comments.project', '-interests.project')
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
@@ -33,11 +31,13 @@ class Project(db.Model, SerializerMixin):
     interest_names = association_proxy('interests', 'interest_name')
     user = db.relationship('User', back_populates='projects')
 
+    serialize_rules = ('-user.projects', '-comments.project', '-interests.project', '-user.interests')
+
 class Interest(db.Model, SerializerMixin):
     __tablename__ = 'interests'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
     user_interests = db.relationship('UserInterest', back_populates='interest')
     project_interests = db.relationship('ProjectInterest', back_populates='interest')
 
@@ -53,7 +53,7 @@ class UserInterest(db.Model, SerializerMixin):
     interest = db.relationship('Interest', back_populates='user_interests')
     interest_name = association_proxy('interest', 'name')
 
-    serialize_rules = ('-user.interests', '-interest.user_interests')
+    serialize_rules = ('-user.interests', '-interest.user_interests', '-user.projects')
 
 class ProjectInterest(db.Model, SerializerMixin):
     __tablename__ = 'project_interest'
@@ -65,7 +65,7 @@ class ProjectInterest(db.Model, SerializerMixin):
     interest = db.relationship('Interest', back_populates='project_interests')
     interest_name = association_proxy('interest', 'name')
 
-    serialize_rules = ('-project.interests', '-interest.project_interests')
+    serialize_rules = ('-project.interests', '-interest.project_interests', '-project.user')
 
 class Comment(db.Model, SerializerMixin):
     __tablename__ = 'comments'
@@ -77,4 +77,4 @@ class Comment(db.Model, SerializerMixin):
     project = db.relationship('Project', back_populates='comments')
     user = db.relationship('User')
 
-    serialize_rules = ('-project.comments', '-user.comments')
+    serialize_rules = ('-project.comments', '-user.projects', '-user.interests')
