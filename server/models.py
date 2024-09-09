558cefe11fea1e2f49208b2e9e728299611fba0a
Ryan <coap.ryanmontgomery@gmail.com>
2024-09-09 13:02:56 -0600

    uupdated error handling and serilialize rules

--- server/models.py
+++ server/models.py
@@ -7,8 +7,8 @@ class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     bio = db.Column(db.String(500))
     avatar = db.Column(db.String(200))
