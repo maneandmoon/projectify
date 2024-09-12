a7d0c0a47c3230c3eba5667b78cd85e5507e9396
LindaTa <lindata@maneandmoon.com>
2024-09-11 21:53:43 -0500

    merged main to linda

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,62 +1,79 @@
-//LoginForm.js
-import React, { useState } from "react";
+import React from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
 import { Button, Error, Input, FormField, Label } from "../styles";
 
-function LoginForm({ onLogin }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+const LoginSchema = yup.object().shape({
+  username: yup.string().required("Username is required"),
+  password: yup.string().required("Password is required"),
+});
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    setIsLoading(true);
-    fetch("/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    }).then((r) => {
-      setIsLoading(false);
-      if (r.ok) {
-        r.json().then((user) => onLogin(user));
-      } else {
-        r.json().then((err) => setErrors(err.errors));
-      }
-    });
-  }
+function LoginForm({ onLogin }) {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: LoginSchema,
+    onSubmit: (values, { setSubmitting, setErrors }) => {
+      setSubmitting(true);
+      fetch("/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      })
+        .then((r) => {
+          setSubmitting(false);
+          if (r.ok) {
+            r.json().then((user) => onLogin(user));
+          } else {
+            r.json().then((err) => setErrors({ api: err.errors }));
+          }
+        });
+    },
+  });
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={formik.handleSubmit}>
       <FormField>
         <Label htmlFor="username">Username</Label>
         <Input
           type="text"
-          id="username"
+          name="username"
           autoComplete="off"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={formik.handleChange}
+          value={formik.values.username}
         />
+        {formik.errors.username && formik.touched.username ? (
+          <Error>{formik.errors.username}</Error>
+        ) : null}
       </FormField>
+
       <FormField>
         <Label htmlFor="password">Password</Label>
         <Input
           type="password"
-          id="password"
+          name="password"
           autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={formik.handleChange}
+          value={formik.values.password}
         />
+        {formik.errors.password && formik.touched.password ? (
+          <Error>{formik.errors.password}</Error>
+        ) : null}
       </FormField>
+
       <FormField>
-        <Button variant="fill" color="primary" type="submit">
-          {isLoading ? "Loading..." : "Login"}
+        <Button variant="fill" color="primary" type="submit" disabled={formik.isSubmitting}>
+          {formik.isSubmitting ? "Loading..." : "Login"}
         </Button>
       </FormField>
+
       <FormField>
-        {errors.map((err) => (
-          <Error key={err}>{err}</Error>
+        {formik.errors.api && formik.errors.api.map((err, index) => (
+          <Error key={index}>{err}</Error>
         ))}
       </FormField>
     </form>
